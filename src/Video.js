8103a4fdee7365e11632bcf3f457d3ff4aead0b3
Dawn <67352124+daabshire@users.noreply.github.com>
2022-07-03 13:42:22 -0400

    Add files via upload

---
+++ src/Video.js
@@ -0,0 +1,47 @@
+import React, { useRef, useState } from "react";
+import "./Video.css";
+import VideoFooter from "./VideoFooter";
+import VideoSidebar from "./VideoSidebar";
+
+function Video( { url, channel, descriptionm, song, likes, messages, shares } ) {
+const [playing, setPlaying] = useState(false);
+const videoRef = useRef(null);
+
+const handleVideoPress = () => {
+    if (playing) {
+        videoRef.current.pause();
+        setPlaying(false);
+    } else {
+        videoRef.current.play();
+        setPlaying(true);
+    }
+};
+
+  return (
+    <div className="video">
+        <video 
+            onClick={handleVideoPress}
+            className="video_player"
+            loop
+            ref={videoRef}
+            src={url}
+            >
+        </video>
+
+
+        <VideoFooter 
+            channel= {channel}
+            description= {descriptionm}
+            song= {song}
+        />
+        <VideoSidebar 
+            likes={likes} 
+            shares={shares} 
+            messages={messages} 
+        />
+
+    </div>
+  );
+} 
+
+export default Video;
